da1c3c1f0b0d4fb2c10046374ac7e6423651d2aa
linqining <linqining@outlook.com>
2026-09-12 06:30:39 +0800

    feat: update gitignore

--- poker-appchain-texasair/target/release/deps/adapter-1cc0af493378a175.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/projects/zchain/poker-appchain-texasair/target/release/deps/adapter-1cc0af493378a175.d: tests/adapter.rs
-
-/Users/mac/projects/zchain/poker-appchain-texasair/target/release/deps/adapter-1cc0af493378a175: tests/adapter.rs
-
-tests/adapter.rs:
